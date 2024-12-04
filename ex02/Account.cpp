539deec877c0d07b7f755a98bcf3da3f4e3477d8
menasy <mehmetnasim42@gmail.com>
2024-12-04 20:43:50 +0300

    CPP00 is finished

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: mehmyilm <mehmyilm@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 15:50:12 by mehmyilm          #+#    #+#             */
-/*   Updated: 2024/12/04 01:31:40 by mehmyilm         ###   ########.fr       */
+/*   Updated: 2024/12/04 16:34:13 by mehmyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ Account::Account(int initial_deposit)
 	_amount = initial_deposit;
 	_totalAmount += _amount;
 	t::_displayTimestamp();
-	std::cout<<"index:"<<_nbAccounts++<<";ammount:"
+	std::cout<<"index:"<<_nbAccounts++<<";amount:"
 			<<t::checkAmount()<<";created"<<std::endl;
 }
 Account::Account(void)
@@ -39,7 +39,7 @@ Account::Account(void)
 Account::~Account(void)
 {
 	t::_displayTimestamp();
-	std::cout<<"index:"<<_accountIndex<<";ammount:"
+	std::cout<<"index:"<<_accountIndex<<";amount:"
 			<<t::checkAmount()<<";closed"<<std::endl;
 }
 int	Account::getNbAccounts(void)
@@ -62,7 +62,7 @@ int	Account::getNbWithdrawals(void)
 void	Account::displayAccountsInfos(void)
 {
 	t::_displayTimestamp();
-	std::cout<<"accounts:"<<t::getNbAccounts()+1
+	std::cout<<"accounts:"<<t::getNbAccounts()
 			<<";total:"<<t::getTotalAmount()
 			<<";deposits:"<<t::_totalNbDeposits
 			<<";withdrawals:"<<t::_totalNbWithdrawals<<std::endl;
@@ -79,9 +79,10 @@ void	Account::_displayTimestamp(void)//ok
 void	Account::makeDeposit(int deposit)//ok
 {
 	t::_displayTimestamp();
+	_amount+=deposit;
 	std::cout<<"index:"<<t::_accountIndex<<";p_amount:"
 			<<t::checkAmount()<<";deposit:"<<deposit
-			<<";amount:"<<(_amount+=deposit)
+			<<";amount:"<<(_amount)
 			<<";nb_deposits:"<<(++_nbDeposits)<<std::endl;
 	_totalAmount += deposit;
 	_totalNbDeposits++;
@@ -89,11 +90,12 @@ void	Account::makeDeposit(int deposit)//ok
 bool	Account::makeWithdrawal(int withdrawal)//ok
 {
 	t::_displayTimestamp();
-	if (_totalAmount >= withdrawal)
+	if (_amount >= withdrawal)
 	{
+		_amount-=withdrawal;
 		std::cout<<"index:"<<t::_accountIndex<<";p_amount:"
 				<<t::_amount<<";withdrawal:"<<withdrawal
-				<<";amount:"<<(_amount-=withdrawal)
+				<<";amount:"<<(_amount)
 				<<";nb_withdrawals:"<<(++t::_nbWithdrawals)<<std::endl;
 		_totalAmount -= withdrawal;
 		_totalNbWithdrawals++;
@@ -111,7 +113,7 @@ int	Account::checkAmount(void) const
 void	Account::displayStatus(void) const
 {
 	t::_displayTimestamp();
-	std::cout<<"index:"<<t::_accountIndex<<";ammount:"
+	std::cout<<"index:"<<t::_accountIndex<<";amount:"
 			<<t::checkAmount()<<";deposits:"
 			<<t::_nbDeposits<<";withdrawals:"
 			<<t::_nbWithdrawals<<std::endl;
