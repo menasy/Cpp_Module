63070d8620f90cea4477cb875e3079058cf98661
Mehmet Nasim Yilmaz <mehmyilm@k1m23s01.42istanbul.com.tr>
2025-01-01 12:56:24 +0300

    Cpp_Module

--- Cpp02/ex02/main.cpp
+++ Cpp02/ex02/main.cpp
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mehmyilm <mehmyilm@student.42istanbul.c    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/01 15:50:12 by mehmyilm          #+#    #+#             */
-/*   Updated: 2024/12/17 00:51:21 by mehmyilm         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/*
+ ███▄ ▄███▓▓█████  ███▄    █  ▄▄▄        ██████▓██   ██▓
+▓██▒▀█▀ ██▒▓█   ▀  ██ ▀█   █ ▒████▄    ▒██    ▒ ▒██  ██▒
+▓██    ▓██░▒███   ▓██  ▀█ ██▒▒██  ▀█▄  ░ ▓██▄    ▒██ ██░
+▒██    ▒██ ▒▓█  ▄ ▓██▒  ▐▌██▒░██▄▄▄▄██   ▒   ██▒ ░ ▐██▓░
+▒██▒   ░██▒░▒████▒▒██░   ▓██░ ▓█   ▓██▒▒██████▒▒ ░ ██▒▓░
+░ ▒░   ░  ░░░ ▒░ ░░ ▒░   ▒ ▒  ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░  ██▒▒▒
+░  ░      ░ ░ ░  ░░ ░░   ░ ▒░  ▒   ▒▒ ░░ ░▒  ░ ░▓██ ░▒░
+░      ░      ░      ░   ░ ░   ░   ▒   ░  ░  ░  ▒ ▒ ░░
+       ░      ░  ░         ░       ░  ░      ░  ░ ░
+                                                ░ ░*/
 
 #include "Fixed.hpp"
 
@@ -16,6 +16,7 @@ int main( void )
 {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed c;
 
 
 	std::cout << a << std::endl;
@@ -28,47 +29,47 @@ int main( void )
 
 	std::cout << Fixed::max( a, b ) << std::endl;
 
-	//std::cout << "====== Values ===="<<std::endl;
-	//std::cout<<"a: ";
-	//std::cout<<a<<std::endl;
-	//std::cout<<"b: ";
-	//std::cout<<b<<std::endl;
-	//std::cout<<"c: ";
-	//std::cout<<c<<std::endl;
-	//
-	//std::cout << "====== Comparison operators ===="<<std::endl;
-	//std::cout<<"< :"<< (b < c) <<std::endl;
-	//std::cout<<"> :"<< (b > c) <<std::endl;
-	//std::cout<<"<= :"<< (b <= c) <<std::endl;
-	//std::cout<<">= :"<< (b >= c) <<std::endl;
-	//std::cout<<"== :"<< (b == c) <<std::endl;
-	//std::cout<<"!= :"<< (b != c) <<std::endl;
-	//
-	//std::cout << "====== Max-Min ===="<<std::endl;
-	//std::cout << Fixed::max( a, b ) << std::endl;
-	//std::cout << Fixed::max( a, c ) << std::endl;
-	//std::cout << Fixed::min( a, b ) << std::endl;
-	//std::cout << Fixed::min( a, c ) << std::endl;
-	//std::cout << Fixed::max( b, c ) << std::endl;
-	//
-	//std::cout << "====== Basic operators ===="<<std::endl;
-	//std::cout<<"+ :"<< (b + c) <<std::endl;
-	//std::cout<<"- :"<< (b - c) <<std::endl;
-	//std::cout<<"/ :"<< (b / c) <<std::endl;
-	//std::cout<<"* :"<< (b * c) <<std::endl;
-	//
-	//
-	//std::cout << "====== Increment (++) and Decrement(--) ===="<<std::endl;
-	//std::cout << a << std::endl;
-	//std::cout << ++a << std::endl;
-	//std::cout << a++ << std::endl;
-	//std::cout << a << std::endl;
-	//
-	//std::cout << c << std::endl;
-	//std::cout << --c << std::endl;
-	//std::cout << c << std::endl;
-	//std::cout << c-- << std::endl;
-	//std::cout << c << std::endl;
+	std::cout << "====== Values ===="<<std::endl;
+	std::cout<<"a: ";
+	std::cout<<a<<std::endl;
+	std::cout<<"b: ";
+	std::cout<<b<<std::endl;
+	std::cout<<"c: ";
+	std::cout<<c<<std::endl;
+	
+	std::cout << "====== Comparison operators ===="<<std::endl;
+	std::cout<<"< :"<< (b < c) <<std::endl;
+	std::cout<<"> :"<< (b > c) <<std::endl;
+	std::cout<<"<= :"<< (b <= c) <<std::endl;
+	std::cout<<">= :"<< (b >= c) <<std::endl;
+	std::cout<<"== :"<< (b == c) <<std::endl;
+	std::cout<<"!= :"<< (b != c) <<std::endl;
+	
+	std::cout << "====== Max-Min ===="<<std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::max( a, c ) << std::endl;
+	std::cout << Fixed::min( a, b ) << std::endl;
+	std::cout << Fixed::min( a, c ) << std::endl;
+	std::cout << Fixed::max( b, c ) << std::endl;
+	
+	std::cout << "====== Basic operators ===="<<std::endl;
+	std::cout<<"+ :"<< (b + c) <<std::endl;
+	std::cout<<"- :"<< (b - c) <<std::endl;
+	std::cout<<"/ :"<< (b / c) <<std::endl;
+	std::cout<<"* :"<< (b * c) <<std::endl;
+	
+	
+	std::cout << "====== Increment (++) and Decrement(--) ===="<<std::endl;
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	
+	std::cout << c << std::endl;
+	std::cout << --c << std::endl;
+	std::cout << c << std::endl;
+	std::cout << c-- << std::endl;
+	std::cout << c << std::endl;
 
 	return 0;
 }
